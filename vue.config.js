6641de208f8ddb59d54cc7087b26ca24b54a28a2
tangjie <tangjie0403@tpl.cntaiping.com>
2019-09-05 16:58:14 +0800

    警告 webpack 的性能提示

--- vue.config.js
+++ vue.config.js
@@ -120,7 +120,21 @@ module.exports = {
       }
       Object.assign(config, {
         optimization
-      })
+      });
+      //警告 webpack 的性能提示
+      Object.assign(config, {
+        performance: {
+          hints: 'warning',
+          //入口起点的最大体积 整数类型（以字节为单位）
+          maxEntrypointSize: 50000000,
+          //生成文件的最大体积 整数类型（以字节为单位 300k）
+          maxAssetSize: 30000000,
+          //只给出 js 文件的性能提示
+          assetFilter: function (assetFilename) {
+            return assetFilename.endsWith('.js');
+          }
+        }
+      });
     }
   },
   // 第三方插件配置 https://www.npmjs.com/package/vue-cli-plugin-style-resources-loader
